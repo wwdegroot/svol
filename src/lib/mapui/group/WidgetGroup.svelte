a975c8ebbcb1f125975a3d1aebe3aaac0a93217e
wwdegroot <wouter.w.de.groot@gmail.com>
2024-12-26 09:47:17 +0100

    update to svelte 5

--- src/lib/mapui/group/WidgetGroup.svelte
+++ src/lib/mapui/group/WidgetGroup.svelte
@@ -1,18 +1,23 @@
 <script lang="ts">
 	import { twMerge } from 'tailwind-merge';
 	import Control from 'ol/control/Control.js';
-	import { getContext, onMount, onDestroy } from 'svelte';
+	import { getContext, onMount, onDestroy, type Snippet } from 'svelte';
 	import type { Writable } from 'svelte/store';
 	import Map from 'ol/Map.js';
 
-	export let position: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
-	let uiControlGroupDiv: HTMLDivElement;
+	interface Props {
+		position: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right';
+		children?: Snippet;
+	}
+
+	let { position, children }: Props = $props();
+	let uiControlGroupDiv: HTMLDivElement | undefined = $state();
 	let uiControlGroupControl: Control;
 	let map: Writable<Map> = getContext('olmap');
 
 	let overlayContainerStopEvent = document.getElementsByClassName('ol-overlaycontainer-stopevent');
 
-	let positionCss = '';
+	let positionCss = $state('');
 	switch (position) {
 		case 'top-left':
 			positionCss = 'left-2 top-2';
@@ -43,5 +48,5 @@
 </script>
 
 <div class={twMerge('absolute ', positionCss)} bind:this={uiControlGroupDiv}>
-	<slot></slot>
+	{@render children?.()}
 </div>
