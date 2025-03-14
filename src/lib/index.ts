e85183f61dca64fd1015ab7e463a93156799ff97
wwdegroot <wouter.w.de.groot@gmail.com>
2025-03-14 13:32:34 +0100

    exports updated

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,4 +1,15 @@
 // Openlayers Map
 export { OpenlayersMap, ResizableOpenlayersMap, createMapStore, createMap, createMapState } from "./olmap/index.js";
+// Projections
+export { rdnewprojection } from "./projection.js";
+// Baselayers PDOK
+export { baselayers, brtStandaard, brtGrijs, brtPastel, brtWater } from './baselayers.js'
+// Sources
+export { WFS, DynamicGeoJson } from './olmap/index.js'
 // BBOX export
 export { BboxExport } from "./bboxexport/index.js";
+// Basic LayerControl
+export { LayerControl } from "./layercontrol/index.js"
+// Identify Features
+export { IdentifyFeatures } from "./identify/index.js"
+export type { LayerField, LayerFields, LayerFieldsMap } from "./identify/index.js"
