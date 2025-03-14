dab49e4f6db8c7efb5a7d4e832412b3d60da54f8
wwdegroot <wouter.w.de.groot@gmail.com>
2025-03-14 13:32:23 +0100

    export sources

--- src/lib/olmap/index.ts
+++ src/lib/olmap/index.ts
@@ -2,3 +2,7 @@ export { default as OpenlayersMap } from "./OpenlayersMap.svelte";
 export { createMapStore } from "./openlayersmap.js";
 export { createMap, createMapState } from "./openlayersmap.svelte.js"
 export { default as ResizableOpenlayersMap } from "./ResizableOpenlayersMap.svelte";
+
+// sources
+export { default as WFS } from './sources/WFS.svelte'
+export { default as DynamicGeoJson } from './sources/geojson/DynamicGeoJsonVector.svelte'
