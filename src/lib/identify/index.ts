397e1ae2083a8e54349d1a508739c065c9db4766
wwdegroot <wouter.w.de.groot@gmail.com>
2025-03-14 14:05:29 +0100

    syntax fix

--- src/lib/identify/index.ts
+++ src/lib/identify/index.ts
@@ -1,2 +1,2 @@
 export { default as IdentifyFeatures } from "./IdentifyFeatures.svelte";
-export type { LayerField, LayerFields, LayerFieldsMap} from './identifyFeaturesTypes.js'
+export type { LayerField, LayerFields, LayerFieldsMap } from './identifyFeaturesTypes.js'
