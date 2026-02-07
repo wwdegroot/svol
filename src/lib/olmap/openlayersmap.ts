62af60287bfc8405808993906dacee4b2a49b2ea
wwdegroot <wouter.w.de.groot@gmail.com>
2026-02-07 08:59:38 +0100

    formatting

--- src/lib/olmap/openlayersmap.ts
+++ src/lib/olmap/openlayersmap.ts
@@ -1,12 +1,12 @@
-import Map, { type MapOptions } from "ol/Map.js";
-import { writable, type Writable } from "svelte/store";
+import Map, { type MapOptions } from 'ol/Map.js';
+import { writable, type Writable } from 'svelte/store';
 
 /**
  * Create a sveltekit store for an openlayers Map
- * 
- * @param {MapOptions} options 
+ *
+ * @param {MapOptions} options
  * @returns sveltekit store with openlayers map
  */
 export function createMapStore(options: MapOptions): Writable<Map> {
-    return writable(new Map(options))
+    return writable(new Map(options));
 }
