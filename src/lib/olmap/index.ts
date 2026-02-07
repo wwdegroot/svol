79ffe065312937d9d976227f36cfcca803d60e0e
wwdegroot <wouter.w.de.groot@gmail.com>
2026-02-07 11:10:22 +0100

    exports added

--- src/lib/olmap/index.ts
+++ src/lib/olmap/index.ts
@@ -1,8 +1,10 @@
-export { default as OpenlayersMap } from "./OpenlayersMap.svelte";
-export { createMapStore } from "./openlayersmap.js";
-export { createMap, createMapState } from "./openlayersmap.svelte.js"
-export { default as ResizableOpenlayersMap } from "./ResizableOpenlayersMap.svelte";
+export const MAPMANAGER_KEY = Symbol('olmapmanager');
 
+export { default as OpenlayersMap } from './OpenlayersMap.svelte';
+export { createMapStore } from './openlayersmap.js';
+export { createMap, createMapState, MapManager } from './openlayersmap.svelte.js';
+export { default as ResizableOpenlayersMap } from './ResizableOpenlayersMap.svelte';
+export { default as OlMapManager } from './OlMapManager.svelte';
 // sources
-export { default as WFS } from './sources/WFS.svelte'
-export { default as DynamicGeoJson } from './sources/geojson/DynamicGeoJsonVector.svelte'
+export { default as WFS } from './sources/WFS.svelte';
+export { default as DynamicGeoJson } from './sources/geojson/DynamicGeoJsonVector.svelte';
