54020fd7b52b4c49dc8dc80d6d1143b5c3f0c515
wwdegroot <wouter.w.de.groot@gmail.com>
2026-02-07 08:57:59 +0100

    new mapmanager constructor

--- src/lib/olmap/openlayersmap.svelte.ts
+++ src/lib/olmap/openlayersmap.svelte.ts
@@ -1,46 +1,115 @@
-import type { View } from "ol";
-import type { Layer } from "ol/layer.js";
-import Map, { type MapOptions } from "ol/Map.js";
+import Map, { type MapOptions } from 'ol/Map.js';
+import type { Collection, Overlay, View } from 'ol';
+import type { Coordinate } from 'ol/coordinate.js';
+import type { Extent } from 'ol/extent.js';
+import type BaseLayer from 'ol/layer/Base.js';
+import type LayerGroup from 'ol/layer/Group.js';
+import TileLayer from 'ol/layer/Tile.js';
+import { OSM } from 'ol/source.js';
+import type { ProjectionLike } from 'ol/proj.js';
 
 /**
  * Create an openlayers Map
- * 
- * @param {MapOptions} options 
+ *
+ * @param {MapOptions} options
  * @returns openlayers map object
  */
 export function createMap(options: MapOptions): Map {
     let map = new Map(options);
-    return map
+    return map;
 }
 
 /**
  * Create an openlayers Map $state
- * 
- * @param {MapOptions} options 
+ *
+ * @param {MapOptions} options
  * @returns openlayers map state object
  */
 export function createMapState(options: MapOptions = {}): Map {
     let map = $state(new Map(options));
-    return map
+    return map;
 }
+
 /**
- * MapState for reactive updates on state change
+ * Map Manager class to access and manage the openlayers map instance with svelte $state runes
  */
-export class MapState {
-    #layers = $state([]) as Array<Layer>
+export class MapManager {
+    #map: Map = $state() as Map;
+    #view: View | undefined = $state(undefined);
+    #layers: Array<BaseLayer> | Collection<BaseLayer> | LayerGroup | undefined = $state(undefined);
+    #overlays: Collection<Overlay> | Array<Overlay> | undefined = $state(undefined);
+    center: Coordinate | undefined = $state(undefined);
+    extent: Extent | undefined = $state(undefined);
+    projection: ProjectionLike = $state() as ProjectionLike;
 
-    set layers(layers: Array<Layer>) {
-        this.#layers = layers
+    constructor(options: MapOptions = {}) {
+        this.#map = new Map(options);
+        this.#view = this.#map.getView();
+        this.#layers = this.#map.getAllLayers();
+        this.#overlays = this.#map.getOverlays();
+        this.center = this.#view?.getCenter();
+        this.extent = this.#view?.calculateExtent();
+        this.projection = this.#view?.getProjection().getCode();
+
+        // set default layer to to OSM
+        if (!options.layers) {
+            let baseLayer = new TileLayer({
+                source: new OSM()
+            });
+            baseLayer.set('layerType', 'base');
+            this.#map.addLayer(baseLayer);
+        }
+
+        // Sync state with map changes
+        this.#view?.on('change:center', () => {
+            this.center = this.#view?.getCenter();
+            this.extent = this.#view?.calculateExtent();
+        });
+
+        this.#map.on('change:view', () => {
+            this.#view = this.#map.getView();
+        });
     }
 
+    // Getters for state
+    get map() {
+        return this.#map;
+    }
+    get view() {
+        return this.#view;
+    }
     get layers() {
-        return this.#layers
+        return this.#layers;
+    }
+    get overlays() {
+        return this.#overlays;
     }
 
-    addLayer(layer: Layer) {
-        this.#layers.push(layer)
+    // Derived state using getters
+    get isInitialized() {
+        return this.#map !== null;
     }
 
-    #map = $state() as Map
-    #view = $state() as View
+    setCenter(coords: Coordinate) {
+        this.view?.setCenter(coords);
+    }
+
+    setExtent(extent: Extent) {
+        this.#view?.fit(extent);
+    }
+
+    setTarget(target: HTMLElement) {
+        this.#map.setTarget(target);
+    }
+
+    setView(view: View) {
+        this.#map.setView(view);
+    }
+
+    destroy() {
+        if (this.#map) {
+            this.#map.setTarget(undefined);
+            this.#map.dispose();
+        }
+    }
 }
